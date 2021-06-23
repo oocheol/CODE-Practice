9808a9c9d742c91a86f7674b3f4fe7be090b0819
oocheol <86232509+oocheol@users.noreply.github.com>
2021-06-24 02:30:06 +0900

    Update 3-3.숫자 카드 게임.py

--- 연습문제/Ch3.그리디/3-3.숫자 카드 게임.py
+++ 연습문제/Ch3.그리디/3-3.숫자 카드 게임.py
@@ -1,18 +1,20 @@
 # 그리디 실전문제 3-3
 # 숫자 카드 게임
 
-n, m = map(int, input("숫자입력(n m) : ").split())
+while True :
+    n, m = map(int, input("숫자입력(n m) : ").split())
+    if n >= 1 and n <= 100 :
+        break
+    if m >= 1 and n <= 100 :
+        break
+
 
 result = 0
 
 for x in range(n) : 
     data = list(map (int, input("숫자입력(m 개) : ").split()))
-        if len(data) :
-             break 
-
+        
     min_data = min(data)
     result = max(result, min_data)
 
 print(result)
-
-s = 211111333
