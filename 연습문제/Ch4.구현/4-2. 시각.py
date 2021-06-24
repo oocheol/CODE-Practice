816f919bfd3c77edc4b4442cb10d8b02944b8e60
oocheol <oocheol@naver.com>
2021-06-24 16:26:22 +0900

    main

--- 연습문제/Ch4.구현/4-2. 시각.py
+++ 연습문제/Ch4.구현/4-2. 시각.py
@@ -1,3 +1,14 @@
 # 구현 실전문제 4-2
 # 시각
 
+h = int(input())
+
+count = 0
+
+for i in range(h + 1) :
+    for j in range(60) :
+        for k in range(60) :
+            if '3' in str(i) + str(j) + str(k) :
+                count += 1
+
+print(count)
