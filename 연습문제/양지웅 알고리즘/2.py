7e334112e800dfce2314005556a55e3676e9e5ab
oocheol <oocheol@naver.com>
2021-06-30 15:40:37 +0900

    main

--- 연습문제/양지웅 알고리즘/2.py
+++ 연습문제/양지웅 알고리즘/2.py
@@ -2,9 +2,14 @@ a, b, R = map(int,input("a, b, N : ").split())
 
 N = int(input("N : "))
 
-for i in range(N) :
-    x, y = map(int, input("x, y : ").split())
-    
+# 리스트 이용하여 한번에 출력
+arr = []
+for _ in range(N) :
+    arr.append(list(map(int,input("x, y : ").split())))
+
+for i in arr :
+    x, y = i
+
     if ((x-a)**2) + ((y-b)**2) >= R**2 :
         print("silent")
 
