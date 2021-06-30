25b10e173cd1aead94bd959b21f973ebb44e15f3
oocheol <oocheol@naver.come>
2021-06-30 21:51:20 +0900

    main

--- 연습문제/Ch4.구현/ddddd.py
+++
@@ -1,8 +0,0 @@
-### array
-
-with open('C:/Users/ooche/OneDrive/바탕 화면/lotto.csv','r', encoding = 'UTF8') as f :
-    for line in f :
-        print(line,end = '')
-
-print("sync check")
-print("sync check")
