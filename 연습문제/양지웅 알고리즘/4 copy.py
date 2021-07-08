36c15d1d582da642c3bc540681ab7f216a877211
oocheol <oocheol@naver.com>
2021-07-08 20:39:39 +0900

    main

--- 연습문제/양지웅 알고리즘/4 copy.py
+++ 연습문제/양지웅 알고리즘/4 copy.py
@@ -5,7 +5,13 @@ num_list = list(map(int, input().split()))
 max_num = num_list[0]
 min_num = num_list[0]
 
-for num in num_list :
-    if max_num < num :
+for num in num_list:
+
+    if max_num < num:
         max_num = num
-    
+
+    if min_num > num:
+        min_num = num
+
+print(max_num)
+print(min_num)
