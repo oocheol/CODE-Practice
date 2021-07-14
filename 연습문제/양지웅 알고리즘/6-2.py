2782414e255cb614265b4ec613f6d9c136116a94
oocheol <oocheol@naver.com>
2021-07-14 16:20:46 +0900

    main

---
+++ 연습문제/양지웅 알고리즘/6-2.py
@@ -0,0 +1,43 @@
+# 6 
+
+que = []
+
+def process_que(x) :
+    if x[0] == "push" :
+        que.append(x[1])
+
+    elif x[0] == "pop" :
+        if len(que) >= 1 :
+            que.reverse()
+            print(que.pop())
+            que.reverse()
+        else :
+            print(-1)
+
+    elif x[0] == "size" :
+        print(len(que))
+
+    elif x[0] == "empty" :
+        if len(que) == 0 :
+            print(1)
+        else :
+            print(0)
+
+    elif x[0] == "front" :
+        if len(que) >= 1 :
+            print(que[0]) 
+        else :
+            print(-1)
+    
+    elif x[0] == "back" :
+        if len(que) >= 1 :
+            print(que[-1]) 
+        else :
+            print(-1)
+
+             
+n = int(input())
+
+for _ in range(n) :
+    x = input().split(" ")
+    process_que(x)
