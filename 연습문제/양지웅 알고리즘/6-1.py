1c0e1c82466797925ab48929538901a25d286dfc
oocheol <oocheol@naver.com>
2021-07-14 16:18:08 +0900

    main

--- 연습문제/양지웅 알고리즘/6-1.py
+++ 연습문제/양지웅 알고리즘/6-1.py
@@ -1,44 +1,61 @@
-# 6 
+# 6-1 class
 
 que = []
 
-class run :
- def process_que(x) :
-    if x[0] == "push" :
-        que.append(x[1])
+class Que :
+    def __init__(self) :
+         self.que = []
+         self.que_size = 0
 
-    elif x[0] == "pop" :
-        if len(que) >= 1 :
-            que.reverse()
-            print(que.pop())
-            que.reverse()
-        else :
-            print(-1)
+    def push(self, num) :
+        self.que.append(int(num))
+        self.que_size += 1
 
-    elif x[0] == "size" :
-        print(len(que))
+    def pop(self) :
+        if self.size() > 0 :
+            self.que_size -= 1
+            return self.que.pop()
 
-    elif x[0] == "empty" :
-        if len(que) == 0 :
-            print(1)
-        else :
-            print(0)
+        return -1
 
-    elif x[0] == "front" :
-        if len(que) >= 1 :
-            print(que[0]) 
-        else :
-            print(-1)
+    def size(self):
+        return self.que_size
+
+    def empty(self):
+        if self.size() > 0:
+            return 0
+        
+        return 1
+
+    def top(self):
+        if self.size() > 0:
+            return self.que[self.size()-1]
+        
+        return -1
+
+def run_cmd_with_que(cmd, stack_lst):
+    cmd_type = cmd[0]
     
-    elif x[0] == "back" :
-        if len(que) >= 1 :
-            print(que[-1]) 
-        else :
-            print(-1)
+    if cmd_type == "push":
+        _, num = cmd
+        que_lst.push(num)
+    elif cmd_type == "pop":
+        print(que_lst.pop())
+    elif cmd_type == "size":
+        print(que_lst.size())
+    elif cmd_type == "empty":
+        print(que_lst.empty())
+    elif cmd_type == "top":
+        print(que_lst.top())
+    
+    return que_lst
 
-             
+que_lst = que()
+# stack_lst.stack
+# stack_lst.stack_size
+
 n = int(input())
 
-for _ in range(n) :
-    x = input().split(" ")
-    process_que(x)
+for _ in range(n):
+    x = input().split()
+    que_lst = run_cmd_with_que(x, que_lst)
