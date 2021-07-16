3c2d9276c536d2d1613474d367be46eff442330a
oocheol <oocheol@naver.com>
2021-07-16 09:51:29 +0900

    main

--- 연습문제/양지웅 알고리즘/6-1.py
+++ 연습문제/양지웅 알고리즘/6-1.py
@@ -10,10 +10,15 @@ class Que :
 
     def pop(self) :
         if self.size() > 0 :
+            self.a = []
+            self.a.append(self.que[0])
+            del self.que[0]
             self.que_size -= 1
-            return self.que.pop()
-
-        return -1
+            return self.a[0]
+             
+                       
+        else :  
+            return -1
 
     def size(self):
         return self.que_size
@@ -24,12 +29,17 @@ class Que :
         
         return 1
 
-    def top(self):
-        if self.size() > 0:
-            return self.que[self.size()-1]
-        
+    def front(self):
+        if self.size() > 0 :
+            return self.que[0]
         return -1
-
+    
+    def back(self):
+        if self.size() > 0 :
+            return self.que[-1]
+        return -1
+    
+    
 def run_cmd_with_que(cmd, stack_lst):
     cmd_type = cmd[0]
     
@@ -40,11 +50,13 @@ def run_cmd_with_que(cmd, stack_lst):
         print(que_lst.pop())
     elif cmd_type == "size":
         print(que_lst.size())
+    elif cmd_type == "front":
+        print(que_lst.front())
+    elif cmd_type == "back":
+        print(que_lst.back())
     elif cmd_type == "empty":
         print(que_lst.empty())
-    elif cmd_type == "top":
-        print(que_lst.top())
-    
+        
     return que_lst
 
 que_lst = Que()
--- 연습문제/양지웅 알고리즘/dd copy.py
+++ 연습문제/양지웅 알고리즘/dd copy.py
@@ -1,5 +1,5 @@
 que = [1,2,3,4,5]
-
+print(que)
 que.reverse()
 print(que.pop())
 print(que)
