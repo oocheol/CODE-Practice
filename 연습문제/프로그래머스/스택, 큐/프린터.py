a9efcb84658e60c70cad713063ffdb01e759f4ed
oocheol <oocheol@naver.come>
2021-07-19 22:57:24 +0900

    main

--- 연습문제/프로그래머스/스택, 큐/프린터.py
+++ 연습문제/프로그래머스/스택, 큐/프린터.py
@@ -1,23 +1,29 @@
-## 스택 / 큐   : 프린터
+
+def solution(pr, loc):
+    res = []
+    idx = []
+    for i, _ in enumerate(pr) :
+        idx.append(i)
+    
+    n = 0
+    
+    while n < len(pr) :
+
+        if pr[0] > max(pr[1:]) :
+            res.append(idx.pop(0))
+            pr.pop(0)
+            n += 1
+
+        else :
+            pr.append(pr.pop(0))
+            idx.append(idx.pop(0))
+            n += 1
+
+    
+    return idx[loc]
 
 a1 = [2, 1, 3, 2]
 a2 = [1, 1, 9, 1, 1, 1]
 
-def solution(pr, loc):
-    
-    c = pr[loc]
-    for i in range(len(pr)) :
-        for z in range(1,len(pr)-1) :
-            if pr[i] < pr[z] :
-                pr.insert(-1,pr[i])
-                pr.remove(pr[i])
-            else :
-                
-            
-    print(pr)
-
-            
-    
-
-solution(a1,2)
-solution(a2,0)
+print(solution(a1,2))
+print(solution(a2,0))
