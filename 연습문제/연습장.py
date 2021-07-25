18425baee5cd46f816fda32679f9a4d6b424e3a8
oocheol <oocheol@naver.come>
2021-07-26 01:02:45 +0900

    21/07/26

--- 연습문제/연습장.py
+++ 연습문제/연습장.py
@@ -1,11 +1,2 @@
-steps = [(-2, -1),(-1, -2),(1, -2),(2, -1),(2, 1),(1, 2),(-1, 2),(-2, 1)]
-
-result = 0 
-
-for step in steps :
-    x = 1 + step[0]
-    y = 1 + step[1]
-    if x >= 1 and x <= 8 and y >= 1 and y <= 8 :
-        result +=1
-    print(x,y)
-print(result)
+while True :
+    print(1)
