5bf3bf57eda8ea7dcf944eb092aa2e594fce2cc4
oocheol <oocheol@naver.come>
2021-08-04 16:31:48 +0900

    21/08/04

--- Code_Test/양지웅 알고리즘/9-1.py
+++ Code_Test/양지웅 알고리즘/9-1.py
@@ -1,2 +1,12 @@
 # 큐 추가 문제(2) - 회전하는 큐
 
+n, m = map(int, input().split())
+a, b, c = map(int,input().split())
+
+array = [x for x in range(1, n+1)]
+print(array)
+temp = [a, b, c]
+
+array1 = array.copy()
+
+    
