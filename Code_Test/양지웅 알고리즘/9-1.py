078e6be1474ef953f88622de61053f6d7dea8375
oocheol <oocheol@naver.come>
2021-08-06 10:46:20 +0900

    21/08/06

--- Code_Test/양지웅 알고리즘/9-1.py
+++ Code_Test/양지웅 알고리즘/9-1.py
@@ -1,12 +1,63 @@
-# 큐 추가 문제(2) - 회전하는 큐
+# 큐 추가문제(2) - 회전하는 큐
 
 n, m = map(int, input().split())
-a, b, c = map(int,input().split())
+d, e, f = map(int,input().split())
 
-array = [x for x in range(1, n+1)]
-print(array)
-temp = [a, b, c]
+a = [z for z in range(1, n+1)]
+print(a)
+x = [d, e, f]
 
-array1 = array.copy()
+f_idx = 0
+b_idx = 0
 
+
+
+for i in range(len(x)-1):
+    if a.index(x[i]) == 0 :
+            a.pop(0)
+            print(a)
+            
     
+    elif a.index(x[i]) <= a.index(x[i+1]) :
+            while True:
+                a.append(a.pop(0))
+                f_idx += 1
+                if a.index(x[i]) == 0 :
+                    a.pop(0)
+                    print(a)
+                    break    
+                
+    elif a.index(x[i]) > a.index(x[i+1]) :
+           while True:
+               a.insert(0,a.pop(-1))
+               b_idx +=1
+               if a.index(x[i]) == 0 :
+                   a.pop(0)
+                   print(a)
+                   break
+    
+    if i == len(x)-2 :
+        if a.index(x[i+1]) == 0 :
+            a.pop(0)
+
+        elif a.index(x[i+1]) < len(a)/2:
+            while True:
+                a.append(a.pop(0))
+                f_idx += 1
+                if a.index(x[i+1]) == 0 :
+                    a.pop(0)
+                    print(a)
+                    break    
+
+        else:
+            while True:
+               a.insert(0,a.pop(-1))
+               b_idx +=1
+               if a.index(x[i+1]) == 0 :
+                   a.pop(0)
+                   print(a)
+                   break
+
+print("f_idx :", f_idx)
+print("b_idx :", b_idx)
+print(a)
