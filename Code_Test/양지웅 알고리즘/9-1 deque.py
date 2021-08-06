b03c871ff547a6ff94a950622f6a3d907fbd6a4f
oocheol <oocheol@naver.com>
2021-08-06 18:22:37 +0900

    21/08/06

--- Code_Test/양지웅 알고리즘/9-1 deque.py
+++ Code_Test/양지웅 알고리즘/9-1 deque.py
@@ -1,59 +1,61 @@
-# 큐 추가문제(2) - 회전하는 큐 - deque
-# deque - ratate 활용 !
+# 큐 추가문제(2) - 회전하는 큐
+# deque
+
+from collections import deque
 
 n, m = map(int, input().split())
 x = list(map(int,input().split()))
 
-a = [z for z in range(1, n+1)]
+a = deque(z for z in range(1, n+1))
 print(a)
 # x = [d, e, f]
 
 f_idx = 0
 b_idx = 0
 
+
 for i in range(len(x)-1):
     if a.index(x[i]) == 0 :
-            a.pop(0)
+            a.popleft()
             print(a)
             
-    
     elif a.index(x[i]) <= a.index(x[i+1]) :
             while True:
-                a.append(a.pop(0))
+                a.append(a.popleft())
                 f_idx += 1
                 if a.index(x[i]) == 0 :
-                    a.pop(0)
+                    a.popleft()
                     print(a)
                     break    
                 
     elif a.index(x[i]) > a.index(x[i+1]) :
            while True:
-               a.insert(0,a.pop(-1))
+               a.appendleft(a.pop())
                b_idx +=1
                if a.index(x[i]) == 0 :
-                   a.pop(0)
+                   a.popleft()
                    print(a)
                    break
     
     if i == len(x)-2 :
         if a.index(x[i+1]) == 0 :
-            a.pop(0)
+            a.popleft()
 
         elif a.index(x[i+1]) < len(a)/2:
             while True:
-                a.append(a.pop(0))
+                a.append(a.popleft())
                 f_idx += 1
                 if a.index(x[i+1]) == 0 :
-                    a.pop(0)
+                    a.popleft()
                     print(a)
                     break    
 
         else:
             while True:
-               a.insert(0,a.pop(-1))
+               a.appendleft(a.pop())
                b_idx +=1
                if a.index(x[i+1]) == 0 :
-                   a.pop(0)
+                   a.popleft()
                    print(a)
                    break
 
