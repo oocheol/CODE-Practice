2b557e5f6aa85477f95891d94b06d771a0637604
oocheol <oocheol@naver.come>
2021-08-11 16:32:46 +0900

    21/08/11

--- Code_Test/양지웅 알고리즘/10.py
+++ Code_Test/양지웅 알고리즘/10.py
@@ -5,3 +5,30 @@
 # 출력예제 1
 # 60466175
 
+whole_num = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+
+# x = 10
+# print(divmod(x, 2))
+# print(list(whole_num))
+
+n, m = input().split()
+def change_num(input_num, num):
+    res = []    
+    result = 0
+    temp_list = list(input_num)
+    temp_list.reverse()
+    for x in temp_list:
+        res.append(whole_num.index(x))
+    for idx, y in enumerate(res):
+        result += num ** idx * y
+    return result
+    # divmod(x, num)
+change_num(n, m)
+
+
+# a = input().split()
+# print(list(a))
+# b = ['zzz']
+# print(list(b[0]))
+
--- Code_Test/프로그래머스/연습장/연습장 copy.py
+++ Code_Test/프로그래머스/연습장/연습장 copy.py
@@ -1,11 +1,5 @@
 a = [1, 2, 3, 4, 1,7]
 b = []
 
-print(a.pop(a.index(min(a))))
-print(a)
-
-# for i in range(len(a)):
-#     b.append(min(a))
-#     a.pop('%d')
-#     print(b)
-# 33
+a = 3
+print(a**0)
