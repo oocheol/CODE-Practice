aa750ce369b3ceb49dcae46745c572b1af7c4329
oocheol <oocheol@naver.com>
2022-01-06 14:41:00 +0900

    22/01/06

--- Code_Test/연습장.py
+++ Code_Test/연습장.py
@@ -1,2 +1,14 @@
-while True :
-    print(12)
+res = []
+i = 1
+for x in range(1, 6) :
+    for j in range(1, 6) :
+        res.append(x * j)
+        if j == 5 :
+            print(res[0], res[1], res[2], res[3], res[4])
+            res = []
+            
+        
+
+
+
+    
